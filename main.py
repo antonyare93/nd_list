785961b8a32627e2735cadfb57e881abb5827040
Antony Arévalo <antonyare_93@hotmail.com>
2024-11-02 16:34:58 -0500

    solution_for_n_dimmentions_GameOfLife_initial_index_list

--- main.py
+++ main.py
@@ -2,23 +2,28 @@ nd_index_list = []
 
 def recalculate_values(nd_indexes: list, initial_value: int):
     has_changed = False
+    index = 1
     while not has_changed:
-        for index, value in enumerate(nd_indexes):
-            if nd_indexes[(index+1)*-1] == 0:
-                
-        print(pos)
-        if pos == 0:
-            pos == initial_value
-            next
-        nd_indexes[index] -= 1
+        if nd_indexes[-index] == 0:
+            nd_indexes[-index] = initial_value
+            index += 1
+            continue
+            
+        nd_indexes[-index] -= 1
+        has_changed = not(has_changed)
+
     return nd_indexes
 
 n = 5
-shape = 100
-init_list = [shape for i in range(n)]
-while not all(init_list) == 0:
+shape = 10
+init_list = [shape-1 for i in range(n)]
+while not all(x == 0 for x in init_list):
     nd_index_list.append(tuple(init_list))
-    print(tuple(init_list))
     init_list = recalculate_values(init_list, shape)
+    if all(x == 0 for x in init_list):
+        nd_index_list.append(tuple(init_list))
 
-print(nd_index_list)
+nd_index_list.reverse()
+with open('./nd_index_list.txt', 'w') as f:
+    f.write(str(nd_index_list))
+
